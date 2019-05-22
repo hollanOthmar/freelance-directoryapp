42c548caf0db6ec7f326b5f6b1963efd08771559
hollan haule <hollanhaule@hollans-MacBook-Pro.local>
2019-05-23 00:06:22 +0100

    cleanup

--- directoryapp/directoryapp/settings/production.py
+++ directoryapp/directoryapp/settings/production.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '&d82uh&p3ielih9f++7*3=$i@%6v%c
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['ITblogs-demo.herokuapp.com','localhost']
+ALLOWED_HOSTS = ['itblogs-demo.herokuapp.com','localhost']
 
 
 # Application definition
